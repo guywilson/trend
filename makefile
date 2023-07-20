defb4fcb14f761cc1edf3c16ee91af38ec7df4b9
Guy Wilson <wilson.guy@gmail.com>
2023-07-20 23:11:48 +0100

    Initial commit

---
+++ makefile
@@ -0,0 +1,68 @@
+###############################################################################
+#                                                                             #
+# MAKEFILE for wctl2                                                          #
+#                                                                             #
+# (c) Guy Wilson 2023                                                         #
+#                                                                             #
+###############################################################################
+
+# Version number for WCTL
+MAJOR_VERSION = 0
+MINOR_VERSION = 1
+
+# Directories
+SOURCE = src
+BUILD = build
+DEP = dep
+
+# What is our target
+TARGET = trend
+
+# Tools
+C = gcc
+LINKER = gcc
+
+# postcompile step
+PRECOMPILE = @ mkdir -p $(BUILD) $(DEP)
+# postcompile step
+POSTCOMPILE = @ mv -f $(DEP)/$*.Td $(DEP)/$*.d
+
+CFLAGS = -c -O2 -Wall -pedantic -g
+DEPFLAGS = -MT $@ -MMD -MP -MF $(DEP)/$*.Td
+
+# Libraries
+STDLIBS =
+EXTLIBS =
+
+COMPILE.c = $(C) $(CFLAGS) $(DEPFLAGS) -o $@
+LINK.o = $(LINKER) $(STDLIBS) -o $@
+
+CSRCFILES = $(wildcard $(SOURCE)/*.c)
+OBJFILES = $(patsubst $(SOURCE)/%.c, $(BUILD)/%.o, $(CSRCFILES))
+DEPFILES = $(patsubst $(SOURCE)/%.c, $(DEP)/%.d, $(CSRCFILES))
+
+all: $(TARGET)
+
+# Compile C/C++ source files
+#
+$(TARGET): $(OBJFILES)
+	$(LINK.o) $^ $(EXTLIBS)
+
+$(BUILD)/%.o: $(SOURCE)/%.c
+$(BUILD)/%.o: $(SOURCE)/%.c $(DEP)/%.d
+	$(PRECOMPILE)
+	$(COMPILE.c) $<
+	$(POSTCOMPILE)
+
+.PRECIOUS = $(DEP)/%.d
+$(DEP)/%.d: ;
+
+-include $(DEPFILES)
+
+install: $(TARGET)
+	cp $(TARGET) /usr/local/bin
+
+clean:
+	rm -r $(BUILD)
+	rm -r $(DEP)
+	rm $(TARGET)
---
+++ src/main.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define READ_BUFFER_SIZE                    64
+
+void printUsage(void) {
+    printf("Usage:\n");
+    printf("\ttrend -s size[K][M][G] file\n\n");
+}
+
+int main(int argc, char ** argv) {
+    int             i;
+    int             fdi;
+    int             fdo;
+    off_t           newLength;
+    size_t          multiplier;
+    char *          pszSizeArg;
+    char *          pszOutputFile;
+    char *          pszFilename;
+    uint8_t         buffer[READ_BUFFER_SIZE];
+    ssize_t         bytesRead;
+
+	if (argc >= 4) {
+		for (i = 1;i < argc - 1;i++) {
+			if (argv[i][0] == '-') {
+				if (argv[i][1] == 's') {
+					pszSizeArg = strdup(&argv[++i][0]);
+				}
+				else if (argv[i][1] == 'o') {
+					pszOutputFile = strdup(&argv[++i][0]);
+				}
+				else {
+					printf("Unknown argument '%s'", &argv[i][0]);
+					printUsage();
+					return 0;
+				}
+			}
+		}
+
+        pszFilename = strdup(&argv[i][0]);
+	}
+	else {
+		printUsage();
+		return -1;
+	}
+
+    if (isalpha(pszSizeArg[strlen(pszSizeArg) - 1])) {
+        switch (pszSizeArg[strlen(pszSizeArg) - 1]) {
+            case 'K':
+                multiplier = 1024;
+                break;
+
+            case 'M':
+                multiplier = 1024 * 1024;
+                break;
+
+            case 'G':
+                multiplier = 1024 * 1024 * 1024;
+                break;
+
+            default:
+                multiplier = 1;
+                break;
+        }
+
+        printf("Multiplier = %ld\n", multiplier);
+        pszSizeArg[strlen(pszSizeArg) - 1] = 0;
+    }
+    else {
+        multiplier = 1;
+    }
+
+    newLength = -strtoll(pszSizeArg, NULL, 10) * multiplier;
+
+    printf("New file length = %lld\n", newLength);
+
+    fdi = open(pszFilename, O_RDONLY);
+
+    lseek(fdi, newLength, SEEK_END);
+
+    fdo = open(pszOutputFile, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+    if (fdo < 0) {
+        printf("ERROR: Failed to open file %s:%s\n", pszOutputFile, strerror(errno));
+        exit(-1);
+    }
+
+    do {
+        bytesRead = read(fdi, buffer, READ_BUFFER_SIZE);
+        write(fdo, buffer, bytesRead);
+    }
+    while (bytesRead > 0);
+
+    if (bytesRead < 0) {
+        printf("ERROR: Failed to read file %s:%s\n", pszFilename, strerror(errno));
+    }
+
+    close(fdi);
+    close(fdo);
+
+    return 0;
+}
